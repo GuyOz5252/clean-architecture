88950de66cfd16b92234f2aea09dd2917947a8a7
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2026-01-24 23:05:37 +0200

    Added logs

--- ChatServer.AppHost/Program.cs
+++ ChatServer.AppHost/Program.cs
@@ -14,4 +14,27 @@ builder.AddProject<Projects.ChatServer>("chat-server")
     .WithReference(postgres)
     .WaitFor(postgres);
 
+var elasticsearch = builder.AddElasticsearch("elasticsearch")
+    .WithDataVolume()
+    .WithEnvironment("ELASTIC_PASSWORD", "Password1")
+    .WithEnvironment("xpack.security.enabled", "false")
+    .WithEnvironment("discovery.type", "single-node");
+
+var kibana = builder.AddContainer("kibana", "docker.elastic.co/kibana/kibana", "8.11.0")
+    .WithHttpEndpoint(port: 5601, targetPort: 5601, name: "kibana")
+    .WithEnvironment("ELASTICSEARCH_HOSTS", "http://elasticsearch:9200")
+    .WithReference(elasticsearch)
+    .WaitFor(elasticsearch);
+
+builder.AddContainer("filebeat", "docker.elastic.co/beats/filebeat", "8.11.0")
+    .WithBindMount("../filebeat.yml", "/usr/share/filebeat/filebeat.yml")
+    .WithBindMount("C:/mnt/Logs", "/app/logs") 
+    .WithEnvironment("ELASTICSEARCH_HOSTS", "http://elasticsearch:9200")
+    .WithEnvironment("ELASTICSEARCH_USERNAME", "elastic")
+    .WithEnvironment("ELASTICSEARCH_PASSWORD", "changeme")
+    .WithArgs("-e", "--strict.perms=false")
+    .WithReference(elasticsearch)
+    .WaitFor(elasticsearch)
+    .WaitFor(kibana);
+
 await builder.Build().RunAsync();
